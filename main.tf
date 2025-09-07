6f5effbaee8cbb2787f4f34f5968903b25a641b9
Viktorpav <doncar2003@gmail.com>
2025-09-07 15:09:15 +0200

    Play book is functionable just issue with etcd mac

--- main.tf
+++ main.tf
@@ -286,10 +286,17 @@ resource "null_resource" "ansible_provisioner" {
   triggers = {
     always_run = "${timestamp()}"
     vm_ids = join(",", concat(
-      [proxmox_vm_qemu.k8s-master-pve.id, proxmox_vm_qemu.k8s-master-pve2.id, proxmox_vm_qemu.k8s-master-pve3.id],
+      [
+        proxmox_vm_qemu.k8s-master-pve.id,
+        proxmox_vm_qemu.k8s-master-pve2.id,
+        proxmox_vm_qemu.k8s-master-pve3.id
+      ],
       proxmox_vm_qemu.k8s-worker-pve[*].id,
       proxmox_vm_qemu.k8s-worker-pve2[*].id,
-      [proxmox_vm_qemu.lb-pve.id, proxmox_vm_qemu.lb-pve2.id]
+      [
+        proxmox_vm_qemu.lb-pve.id,
+        proxmox_vm_qemu.lb-pve2.id
+      ]
     ))
   }
 
@@ -299,6 +306,12 @@ resource "null_resource" "ansible_provisioner" {
       echo "master-1 ansible_host=192.168.0.101 ansible_user=ubuntu ansible_ssh_private_key_file=~/Projects/.ssh/id_ed25519" >> inventory.ini
       echo "master-2 ansible_host=192.168.0.102 ansible_user=ubuntu ansible_ssh_private_key_file=~/Projects/.ssh/id_ed25519" >> inventory.ini
       echo "master-3 ansible_host=192.168.0.103 ansible_user=ubuntu ansible_ssh_private_key_file=~/Projects/.ssh/id_ed25519" >> inventory.ini
+      echo "" >> inventory.ini
+      echo "[etcd_nodes]" >> inventory.ini
+      echo "master-1 ansible_host=192.168.0.101 ansible_user=ubuntu ansible_ssh_private_key_file=~/Projects/.ssh/id_ed25519" >> inventory.ini
+      echo "master-2 ansible_host=192.168.0.102 ansible_user=ubuntu ansible_ssh_private_key_file=~/Projects/.ssh/id_ed25519" >> inventory.ini
+      echo "master-3 ansible_host=192.168.0.103 ansible_user=ubuntu ansible_ssh_private_key_file=~/Projects/.ssh/id_ed25519" >> inventory.ini
+      echo "mac-etcd ansible_host=192.168.0.12 ansible_connection=local" >> inventory.ini
       echo "" >> inventory.ini
       echo "[k8s_workers]" >> inventory.ini
       echo "worker-pve-1 ansible_host=192.168.0.110 ansible_user=ubuntu ansible_ssh_private_key_file=~/Projects/.ssh/id_ed25519" >> inventory.ini
